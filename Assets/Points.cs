a83c13fc215ce22d731b3cea8886a221376a515a
RaulAdriano <raul.adriano.ramos@gmail.com>
2022-11-22 02:25:05 -0300

    game base

---
+++ Assets/Points.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    public GameController controller;
+
+    private void Start()
+    {
+        controller = FindObjectOfType<GameController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D colisor)
+    {
+        controller.score++;
+        controller.scoreText.text = controller.score.ToString();
+    }
+}
